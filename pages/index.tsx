897a177c66b0205ccbaf74e4044150491312044a
ashkl <asshwin12@yahoo.co.uk>
2022-09-26 23:57:38 +0100

    added dark/light mode toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
-import { Box, Center, useColorModeValue } from '@chakra-ui/react'
-import type { NextPage } from 'next'
-import ProfileCard from '../Components/ProfileCard/ProfileCard'
+import { Box, Center, Stack, useColorModeValue } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import ProfileCard from "../Components/ProfileCard/ProfileCard";
+import ThemeSwitch from "../Components/Utl/ThemeSwitch";
 
 const Home: NextPage = () => {
-
   return (
-    < Box minH="100vH" bg={useColorModeValue('gray.100', 'black')}>
-      <Center px={3} display='flex' justifyContent={'center'} alignItems='center' textAlign='center' minHeight={'96vh'} className="profile-card">
-        <ProfileCard />
+    <Box minH="100vH" bg={useColorModeValue("gray.100", "black")}>
+      <Center
+        px={3}
+        display="flex"
+        justifyContent={"center"}
+        alignItems="center"
+        textAlign="center"
+        minHeight={"96vh"}
+        className="profile-card"
+      >
+        <Stack>
+          <ProfileCard />
+          <ThemeSwitch />
+        </Stack>
       </Center>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
