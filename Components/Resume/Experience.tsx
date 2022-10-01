b5db3fb29de95e4cd47cc7fa95add4007573888d
ashkl <asshwin12@yahoo.co.uk>
2022-10-01 03:45:49 +0100

    added experience styles and panel

--- Components/Resume/Experience.tsx
+++ Components/Resume/Experience.tsx
@@ -1,7 +1,36 @@
-import { Heading, TabPanel } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { Heading, TabPanel, Text } from "@chakra-ui/react";
 import React from "react";
+import ExpData from "../../Data/experienceData.json";
+import styles from "./Experience.module.css";
 
 export default function Experience() {
-  return <TabPanel></TabPanel>;
+  return (
+    <TabPanel>
+      <div className={styles.container}>
+        <div className="container">
+          <ul>
+            {ExpData.slice(0)
+              .reverse()
+              .map((item) => (
+                <li>
+                  <span></span>
+                  <Heading
+                    textAlign={"left"}
+                    fontFamily={"body"}
+                    fontSize={"3xl"}
+                  >
+                    {item.title}
+                  </Heading>
+                  <Text textAlign={"left"}>{item.desc}</Text>
+                  <div className="dates">
+                    <span>{item.date.start}</span>
+                    <span>{item.date.end}</span>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </TabPanel>
+  );
 }
