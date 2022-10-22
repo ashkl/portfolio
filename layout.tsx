917cd1f8c0abdf75de96817693fc23d075a48d6f
ashkl <asshwin12@yahoo.co.uk>
2022-10-22 17:49:40 +0100

    reformatted layout page

--- layout.tsx
+++ layout.tsx
@@ -1,17 +1,41 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import Head from "next/head"
-import theme from "./theme"
+import {
+  Box,
+  Center,
+  ColorModeScript,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Head from "next/head";
+import ThemeSwitch from "./Components/Utl/ThemeSwitch";
+import theme from "./theme";
 
 export default function Layout({ children }: any) {
-    return (
-        <div>
-            <Head>
-                <title>ASSHWIN.M</title>
-            </Head>
-            <div>
-                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <main>{children}</main>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>ASSHWIN.M</title>
+      </Head>
+      <div>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <main>
+          <Box minH="100vH" bg={useColorModeValue("gray.100", "black")}>
+            <Center
+              px={3}
+              display="flex"
+              justifyContent={"center"}
+              alignItems="center"
+              textAlign="center"
+              minHeight={"100vh"}
+              className="profile-card"
+            >
+              <Stack>
+                {children}
+                <ThemeSwitch />
+              </Stack>
+            </Center>
+          </Box>
+        </main>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,7 @@ import ProfileCard from "../Components/ProfileCard/ProfileCard";
 import ThemeSwitch from "../Components/Utl/ThemeSwitch";
 
 const Home: NextPage = () => {
-  return (
-    <Box minH="100vH" bg={useColorModeValue("gray.100", "black")}>
-      <Center
-        px={3}
-        display="flex"
-        justifyContent={"center"}
-        alignItems="center"
-        textAlign="center"
-        minHeight={"100vh"}
-        className="profile-card"
-      >
-        <Stack>
-          <ProfileCard />
-          <ThemeSwitch />
-        </Stack>
-      </Center>
-    </Box>
-  );
+  return <ProfileCard />;
 };
 
 export default Home;
